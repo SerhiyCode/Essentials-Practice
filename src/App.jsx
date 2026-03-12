d4df7bf0c8b42dd04cdc20d87ddb0ce71601c1de
Serhiy <seregarachuk20@gmail.com>
2026-03-12 17:02:33 +0200

    Adding a header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Header from "./componets/Header"
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <Header />
   )
 }
 
---
+++ src/componets/Header.jsx
@@ -0,0 +1,8 @@
+import  logo from '../assets/investment-calculator-logo.png';
+
+export default function() {
+    return <header id='header'> 
+        <img src={logo} alt="Logo showing a money" /> 
+        <h1>Investment Calculator</h1>
+    </header>
+}
