59d6f24337c1165a24a4ffb004d4e8c47b1ebccc
Serhiy <seregarachuk20@gmail.com>
2026-03-13 11:48:33 +0200

    Handling Events  using two way-binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import Header from "./componets/Header"
+import Header from "./components/Header"
+import UserInput from "./components/UserInput"
 
 function App() {
-  return (
-    <Header />
+  return ( 
+    <> 
+     <Header /> 
+     <UserInput /> 
+    </>
   )
 }
 
